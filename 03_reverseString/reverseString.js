89d20b369add76a10d43c7656dbf296ef89a2b42
selogan <40747212+selogan@users.noreply.github.com>
2025-01-07 15:10:10 -0600

    Add completed third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // return string.split("").reverse().join("")
+    let reversed = "";
+    for (let i = string.length - 1; i >= 0; i--){
+        reversed += string.at(i);
+    }
+    return reversed;
 };
 
 // Do not edit below this line
