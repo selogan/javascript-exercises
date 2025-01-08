c514163305bf1f331ea93460176175399ed4ce5b
selogan <40747212+selogan@users.noreply.github.com>
2025-01-07 18:17:27 -0600

    Add completed fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if ((!Number.isInteger(from) || !Number.isInteger(to)) || (from < 0 || to < 0)) {
+        return "ERROR";
+    }
 
+    if (from > to) [from, to] = [to, from];
+
+    let sum = 0;
+    for (let i = from; i <= to; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
